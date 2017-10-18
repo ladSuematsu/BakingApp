3d8fb5c3d389e0195fae11e6db2fbce334e82d56
Cristhian Suematsu <suematsu01@gmail.com>
2017-10-17 22:00:53 -0200

    Integrate database repositories into widget

--- app/src/main/java/com/ladsoft/bakingapp/mvp/presenter/state/RecipeState.kt
+++ app/src/main/java/com/ladsoft/bakingapp/mvp/presenter/state/RecipeState.kt
@@ -1,19 +1,30 @@
 package com.ladsoft.bakingapp.mvp.presenter.state
 
 import android.os.Bundle
+import com.ladsoft.bakingapp.application.BakingAppApplication
+import com.ladsoft.bakingapp.manager.SessionManager
 import com.ladsoft.bakingapp.mvp.RecipeMvp
 import java.io.Serializable
+import javax.inject.Inject
 
 
 class RecipeState: RecipeMvp.StateContainer {
     override var recipeId : Long = 0L
+    @Inject lateinit var sessionManager: SessionManager
+
+    init {
+        BakingAppApplication.appComponent.inject(this)
+    }
 
     constructor(bundle: Bundle?) {
-        recipeId = bundle?.getLong(EXTRA_RECIPE_ID) ?: 0L
+        (bundle?.getLong(EXTRA_RECIPE_ID) ?: 0L).apply {
+            recipeId = this
+            sessionManager.setLastSelectedReceiptId(this)
+        }
     }
 
     constructor(serializable: Serializable?) {
-        if (serializable != null) {
+        serializable?.apply {
             val stateMap = serializable as HashMap<*, *>
             recipeId = stateMap[EXTRA_RECIPE_ID] as Long
         }
