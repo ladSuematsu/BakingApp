d4777040982c748a72a176ec0a57441ae24e9c02
Cristhian Juniti Suematsu <suematsu01@gmail.com>
2018-05-29 22:51:25 -0300

    Refactor Database Repository classes to Java code

---
+++ app/src/main/java/com/ladsoft/bakingapp/data/database/repository/DatabaseRecipeRepository.java
@@ -0,0 +1,63 @@
+package com.ladsoft.bakingapp.data.database.repository;
+
+import com.ladsoft.bakingapp.data.database.dao.RecipeDao;
+import com.ladsoft.bakingapp.data.database.entity.RecipeRecord;
+import com.ladsoft.bakingapp.data.database.translator.RecipeRecordTranslator;
+import com.ladsoft.bakingapp.data.database.translator.RecipeTranslator;
+import com.ladsoft.bakingapp.entity.Recipe;
+import com.ladsoft.bakingapp.translator.Translator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseRecipeRepository {
+    private final RecipeDao dao;
+
+    private final Translator<Recipe, RecipeRecord> recipeRecordTranslator;
+    private final Translator<RecipeRecord, Recipe> recipeTranslator;
+
+    public DatabaseRecipeRepository(RecipeDao dao) {
+        this.dao = dao;
+        this.recipeRecordTranslator = new RecipeRecordTranslator();
+        this.recipeTranslator = new RecipeTranslator();
+    }
+
+    public Recipe loadRecipe(long recipeId)  {
+        RecipeRecord record = dao.getById(recipeId);
+        return record != null ? recipeTranslator.translate(record) : null;
+    }
+
+    public List<Recipe> loadRecipes() {
+        List<Recipe> result = new ArrayList<>();
+
+        List<RecipeRecord> records = dao.getAll();
+        for (RecipeRecord record : records) {
+            result.add(recipeTranslator.translate(record));
+        }
+
+        return result;
+    }
+
+    public void insert(List<Recipe> recipes) {
+        List<RecipeRecord> records = new ArrayList<>();
+
+        for (Recipe recipe : recipes) {
+            records.add(recipeRecordTranslator.translate(recipe));
+        }
+
+        dao.add(records);
+    }
+
+    public void insert(Recipe recipe) {
+        RecipeRecord record = recipeRecordTranslator.translate(recipe);
+        dao.add(record);
+    }
+
+    public void deleteAll() {
+        dao.deleteAll();
+    }
+
+    public void deletePreserving(List<Long> preserveIds){
+        dao.deletePreserving(preserveIds);
+    }
+}
